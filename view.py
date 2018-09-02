e7f9c98d3c9b542009ef0af83037b513a2827874
Michael Hines <michael.hines@yale.edu>
2018-09-02 09:55:44 -0400

    can select section in the view to print info about that section

--- view.py
+++ view.py
@@ -1,12 +1,12 @@
 from neuron import h, gui
-from cellorg import org2gid, xyz, p, nlayer, ncircle, npts
+from cellorg import org2gid, xyz, p, nlayer, ncircle, npts, gid2org
 
 class View():
   def __init__(self, pts):
     diam = p.cell_diameter
     self.pts = pts
-    self.sections = []
-    self.sl =h. SectionList()
+    self.sections = {}
+    self.sl =h.SectionList()
     for pt in pts:
       gid = org2gid(*pt)
       sec = h.Section(name=str(gid))
@@ -15,10 +15,25 @@ class View():
       sec.pt3dadd(x, y, z, diam)
       x,y,z = xyz(pt[0], pt[1], pt[2] + 1)
       sec.pt3dadd(x, y, z, diam)
-      self.sections.append(sec)
+      self.sections[sec] = gid
       self.sl.append(sec=sec)
     self.sh = h.Shape(self.sl)
+    self.sh.menu_tool("print info", self.callback)
 
+  def callback(self, type, x, y, keystate):
+    #info about nearest section to mouse
+    if type == 2:
+      s = self.sh
+      d = s.nearest(x, y)
+      arc = s.push_selected()
+      if arc >= 0:
+        s.select()
+        sec = h.cas()
+        gid = self.sections[sec]
+        ilayer, icircle, ipt = gid2org(gid)
+        x,y,z = xyz(ilayer, icircle, ipt)
+        print ("gid %d   id (%d, %d, %d) prox pt at (%g, %g, %g) length %g"%(gid, ilayer, icircle, ipt, x, y, z, sec.L))
+        h.pop_section()
 
 def test1():
   pts = []
