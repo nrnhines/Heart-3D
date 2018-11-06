4fe26aec43f83dd0b065dabd4e8a028627acd58b
Michael Hines <michael.hines@yale.edu>
2018-11-06 15:22:28 -0500

    constant distance between circles in all layers

--- cellorg.py
+++ cellorg.py
@@ -1,5 +1,6 @@
 import param as p
-from morphdef import circle_origins, outer_layer_origins, distance
+from morphdef import circle_origins, outer_layer_origins
+from morphdef import distance, const_sep_layer_origins, addmul, normgrad
 from math import pi, cos, sin, tan
 
 # structure that allows fast calculation of cell position and neighbors
@@ -27,9 +28,46 @@ def morphorg():
   zend = p.nominal_height
   paraboloid = [circle_origins(sep, rstart, zend)]
   for i in range(1, p.n_layer):
-    paraboloid.append(outer_layer_origins(i, paraboloid[0]))
+    #paraboloid.append(outer_layer_origins(i, paraboloid[0]))
+    paraboloid.append(const_sep_layer_origins(i, sep, paraboloid[0][0], zend))
   npts = [[circle0_n(pt) for pt in paraboloid[i]] for i in range(p.n_layer)]
   return paraboloid, npts
+
+pt2circle_maxiter = 0
+
+def pt2circle(ilayer, pt):
+  global pt2circle_maxiter
+  # given a point in a layer, what is the icircle that contains the point
+  # Note that an icircle domain is from -sep/2 to +sep/2.
+  assert(pt[1] == 0.0) # assume x, z on the layer surface.
+  circles = paraboloid[ilayer]
+  sep2 = p.internal_surface_circle_distance * .5
+  # kind of a discrete newton method
+  # the dz between circles is an increasing function of z so start at end
+  i = len(circles) - 1
+  z = circles[i][2]
+  if pt[2] >= z:
+    assert(pt[2] <= z + sep2)
+    return i
+  maxiter = 0
+  while i > 0 and pt[2] < z:
+    dz = z - circles[i-1][2]
+    j = int((pt[2] - z)/dz)
+    i += j if j else -1
+    z = circles[i][2]
+    assert(pt[2] < circles[i+1][2])
+    maxiter += 1
+    assert (maxiter < 20)
+  if maxiter > pt2circle_maxiter:
+    pt2circle_maxiter = maxiter
+  if i == 0 and pt[2] < z :
+    assert(pt[2] >= z - sep2)
+    return i
+  d1 = distance(pt, circles[i])
+  d2 = distance(pt, circles[i+1])
+  if (d1 > d2):
+    i += 1
+  return i
 
 paraboloid, npts = morphorg()
 nlayer = len(paraboloid)
@@ -255,10 +293,34 @@ def test4():
     h.Vector(d_circle).line(g1, h.Vector(d_surf), j+1, 2)
   return g1, g2
 
+def test5(): #pt2circle
+  from neuron import h, gui
+  o0 = paraboloid[0]
+  n = 4
+  o1 = paraboloid[n]
+  d = distance(o1[0], o0[0])
+  g = h.Graph()
+  for p in o1:
+    g.mark(p[0], p[2], "|", 10, 1, 1)
+  for p in o0:
+    p1 = addmul(p, d, normgrad(p))
+    i = pt2circle(n, p1)
+    g.mark(p1[0], p1[2], "|", 10, 2, 1)
+    g.mark(o1[i][0], o1[i][2], "|", 10, 3, 1)
+    d1 = distance(p1, o1[i])
+    if i > 0 and p1[2] < o1[i][2]:
+      d2 = distance(p1, o1[i-1])
+      print(i, " < ", distance(p1, o1[i]), distance(p1, o1[i-1]))
+      assert (d1 <= d2)
+    if i < len(o1)-1 and p1[2] > o1[i][2]:
+      d2 = distance(p1, o1[i+1])
+      print(i, " > ", distance(p1, o1[i]), distance(p1, o1[i+1]))
+      assert (d1 <= d2)
+
 if __name__ == "__main__":
  
   #test1()
-  if True:
+  if False:
     c = 1
     n = npts[0][c]
     test2(1, c, 0)
@@ -267,8 +329,8 @@ if __name__ == "__main__":
     test2(1, c, 1)
     test2(1, c, n-2)
 
-  test3(1, 0, 1)
-  test3(4, 101, 100)
+  #test3(1, 0, 1)
+  #test3(4, 101, 100)
 
   #a = test4()
-
+  a = test5()
